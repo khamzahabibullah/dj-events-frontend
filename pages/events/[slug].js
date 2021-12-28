b710827fc6d7222ebe18755b0b27088ebc2d0f54
Khamza <habibullahkhamza@gmail.com>
2021-12-28 14:15:31 +0400

    layout set

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,10 +1,10 @@
-import {useRouter} from 'next/router';
+import Layout from '../../components/Layout';
 
 const EventPage = () => {
   return (
-    <div>
+    <Layout>
       <h1>My Event</h1>
-    </div>
+    </Layout>
   );
 };
 
